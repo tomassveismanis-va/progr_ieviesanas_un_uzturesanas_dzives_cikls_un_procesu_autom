456a09861c2434669e024c466e22482ebad5c1fb
tomassveismanis-va <tomass.veismanis@va.lv>
2026-09-10 19:52:06 +0300

    feat: add POST /tasks endpoint

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const taskModel = require('./models/taskModel');
 const app = express();
 app.use(express.json());
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
-
+app.post('/tasks', async (req, res) => {
+  const [task] = await taskModel.create(req.body);
+  res.status(201).json(task);
+});
 app.listen(3000, () => console.log('Server running on port 3000'));
 
 module.exports = app;
