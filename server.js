004d7eae8ae84544a510fb631ab0016a6c5d4e8b
tomassveismanis-va <tomass.veismanis@va.lv>
2026-09-10 19:56:12 +0300

    feat: add GET /tasks/:id endpoint

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/tasks', async (req, res) => {
   const tasks = await taskModel.getAll();
   res.json(tasks);
 });
+app.get('/tasks/:id', async (req, res) => {
+  const task = await taskModel.getById(req.params.id);
+  res.json(task);
+});
 app.listen(3000, () => console.log('Server running on port 3000'));
 
 module.exports = app;
