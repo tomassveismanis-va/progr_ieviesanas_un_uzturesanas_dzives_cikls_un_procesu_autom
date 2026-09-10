c6ee7b67a8a15db2352ad92f047bb6c2492f0509
tomassveismanis-va <tomass.veismanis@va.lv>
2026-09-10 20:12:59 +0300

    test: add test for GET /tasks

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -4,4 +4,8 @@ const app = require('../server');
 test('POST /tasks creates a task', async () => {
   const res = await request(app).post('/tasks').send({ title: 'Test' });
   expect(res.statusCode).toBe(201);
+test('GET /tasks returns array', async () => {
+  const res = await request(app).get('/tasks');
+  expect(Array.isArray(res.body)).toBe(true);
+});
 });
