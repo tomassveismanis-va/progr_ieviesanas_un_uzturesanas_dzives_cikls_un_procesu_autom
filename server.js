491084d5d41a541e48bd9a6a1e360c48ce64d6e0
tomassveismanis-va <tomass.veismanis@va.lv>
2026-09-10 18:51:05 +0300

    feat: add basic Express server

---
+++ server.js
@@ -0,0 +1,8 @@
+javascript
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => console.log('Server running on port 3000'));
+
+module.exports = app;
